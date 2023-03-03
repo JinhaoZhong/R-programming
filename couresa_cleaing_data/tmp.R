77890b6fb9afcc0647cb06d9b49fdf075f65ceae
Jinhao Zhong <51806816+JinhaoZhong@users.noreply.github.com>
2023-03-02 23:11:26 -0600

    Add files via upload

--- couresa_cleaing_data/tmp.R
+++ couresa_cleaing_data/tmp.R
@@ -1,68 +1,48 @@
-wkdir = "C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset"
-setwd(wkdir)
+# Read data from files
+x_train <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+x_test <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+y_train <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt")
+y_test <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
+subject_train <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt")
+subject_test <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt")
+features <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
+activity_labels <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
 
-# create folder
-if (!file.exists("./data")) {dir.create("./data")}
+# Find mean and std 
+filter <- grep("-mean|-std", features$V2)
+features_filtered <- features[filter,]
 
-# Downloaded data file if it is not already ther
-if(!file.exists("./data/GCDcourseproject.zip")) {download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "./data/GCDcourseproject.zip")}
+# Combine training and testing data sets
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
+x_filtered <- x[,filter]
+subject <- rbind(subject_train, subject_test)
 
-# Unzip file 
-if(!file.exists("./data/UCI HAR Dataset/")) {unzip("./data/GCDcourseproject.zip", exdir = "./data")}
+# Combine columns of subject and y
+subject_y = cbind(subject, y)
 
-# assignment only asks for means and standard deviations
-feat <- read.table("./data/UCI HAR Dataset/features.txt", header = F, sep=" ", as.is=T) 
-keep <- grepl(pattern = "mean|std", x = feat$V2)==TRUE & grepl(pattern = "Freq", x=feat$V2)==FALSE
-featkeep <- feat [keep==T,]
+# Create an extra column for merging
+subject_y$id  <- 1:nrow(subject_y)
+colnames(subject_y) <- c("subject id", "activity id", "id")
 
-# Load X files 
-library(readr)
-wd <- fwf_widths (rep(16, 561))
-trdata <- read_fwf ("./data/UCI HAR Dataset/train/X_train.txt", col_positions =  wd ,  n_max = 7352)
-trdata <- trdata[, keep==TRUE] #only keep the mean and std
+# Merge subject_y and activity_labels
+merged <- merge(subject_y, activity_labels, by.x = "activity id", by.y = "V1")
+activity <- merged[order(merged$id),][,-3]
 
-testdata <- read_fwf ("./data/UCI HAR Dataset/test/X_test.txt", col_positions =  wd ,  n_max = 2947)
-testdata <- testdata[, keep==TRUE]
+# Combine columns of activity and x_filtered
+df <- cbind(activity, x_filtered)
 
+# Reorder first and second columns
+df <- df[,c(2,1,3:ncol(df))]
 
-# add the type of activity
-library(dplyr)
-activity_type <- read.table("./data/UCI HAR Dataset/activity_labels.txt") 
+# Create descriptive names
+colnames(df) <- c("subject id", "activity id", "activity", features_filtered$V2)
 
-tractivity <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/data/UCI HAR Dataset/train/y_train.txt") 
-testactivity <- read.table("C:/Users/ZJH_8/Desktop/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/data/UCI HAR Dataset/train/X_train.txt") 
+# Calculate the mean of each filtered feature
+df_aggregated <- aggregate(df[4:ncol(df)], df[1:3], FUN = mean)
 
-trsub <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
-testsub <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+# Order result by subject id and activity id
+result <- df_aggregated[order(df_aggregated$`subject id`, df_aggregated$`activity id`),]
 
-
-trmerge <- trdata %>% bind_cols(tractivity) %>%
-  full_join (activity_type) %>% bind_cols(trsub)
-
-testmerge <- testdata %>% bind_cols(testactivity) %>%
-  full_join (activity_type) %>% bind_cols(testsub)
-
-alldata <- rbind (trmerge, testmerge)
-
-colnames(alldata) <- c(featkeep$V2, "activity", "activity_type", "subject")
-
-#Summarize to make tidy data
-library(tidyr)
-
-meanslong <- alldata %>%
-  gather(variable, value, -(c(activity, activity_type, subject))) %>%
-  separate(variable, into=c("measurement_type", "summary_type", "axis"), sep="-", remove =T) %>%
-  separate(measurement_type, into=c("domain_signal", "signal_type") , 1 ,remove=T) %>%
-  group_by(subject, activity_type, domain_signal, signal_type, summary_type, axis) %>%
-  summarize (mean(value)) %>%
-  spread("summary_type", "mean(value)") %>%
-  select(subject:axis, mean_val = starts_with("mean"), std_val= starts_with("std"))
-
-meanshort <- alldata %>%
-  gather(variable, value, -(c(activity, activity_type, subject))) %>%
-  group_by(subject, activity_type, variable) %>%
-  summarize (mean(value)) %>%
-  spread(variable, "mean(value)")
-
-# result
-write.table(meanshort, "output.txt", row.names = F)
+# Create a tidy data set file
+write.table(result, "result.txt", row.names = FALSE)
